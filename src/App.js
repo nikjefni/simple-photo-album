ec7ea3371bd7da5a888453c265e6928662425932
jeff <nikjefni@gmail.com>
2019-12-10 08:36:04 +0800

    added comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
+//Main component of the app
+//Display the child component
 import React from 'react';
-// import './App.css';
 import './style.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRedo } from '@fortawesome/free-solid-svg-icons'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col } from 'react-bootstrap'
 import PhotoWrapper from './PhotoWrapper';
-// const photoGen = require('./_App');
 
 class App extends React.Component {
 
